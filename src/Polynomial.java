ae445d6fc9300d3c15e6d1dd030ff2dc879e8695
srdesai <isriramdesai@gmail.com>
2018-08-07 08:54:47 +0530

    Init.

---
+++ src/Polynomial.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class Polynomial {
+
+    private List<Integer> coefficients;
+
+    public Polynomial(String poly) {
+
+    }
+
+    public Polynomial add(Polynomial p2) {
+        return new Polynomial("as");
+    }
+
+    public Polynomial subtract(Polynomial p2) {
+        return new Polynomial("as");
+    }
+
+    public Polynomial multiply(Polynomial p2) {
+        return new Polynomial("as");
+    }
+
+}
